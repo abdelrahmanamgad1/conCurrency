844a79c16cbb2aec10098dd9d31515e9b6a5544a
abdelrahman amgad <abdelrhmanamggad14@gmail.com>
2023-08-29 16:37:14 +0300

    stylingFinal

--- src/app/components/convert/convert.component.ts
+++ src/app/components/convert/convert.component.ts
@@ -1,13 +1,10 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
   FormGroup,
-  FormControl,
   Validators,
   FormBuilder,
 } from '@angular/forms';
-import { Data } from '@angular/router';
-import { ConvCurrency, Currency } from 'src/app/core/interfaces/currency.model';
-import { ApiService } from 'src/app/core/services/api.service';
+import {ApiService} from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-convert',
@@ -15,31 +12,21 @@ import { ApiService } from 'src/app/core/services/api.service';
   styleUrls: ['./convert.component.scss'],
 })
 export class ConvertComponent implements OnInit {
-  countries: Data[] = [];
   exform!: FormGroup;
   conversionOutput!: number;
-  rand1Country={code: 'GBP', name: 'England', flagUrl: 'https://flagsapi.com/GB/flat/64.png'}
-  ;
-  rand2Country={code: 'BHD', name: 'Bahrain', flagUrl: 'https://flagsapi.com/BH/flat/64.png'}
 
-  selectedFirst: string = 'undefined';
-  selectedItem = {code: 'GBP', name: 'England', flagUrl: 'https://flagsapi.com/GB/flat/64.png'}
-
-    constructor(private apiService: ApiService, private fb: FormBuilder) {}
+  constructor(public apiService: ApiService, private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
     this.exform = this.fb.group({
       value: [null, [Validators.required]],
       srcCountry: ['', [Validators.required]],
       dstCountry: ['', [Validators.required]],
-      amount: ['', { value: '', disabled: true }],
-
+      amount: ['', {value: '', disabled: true}],
     });
-
-    this.apiService.getCurrencyApi().subscribe((response) => {
-      this.countries = response;
-      console.log('-> this.countries', this.countries);
-    });
+    this.exform.get('srcCountry')?.setValue(this.apiService.countries[1])
+    this.exform.get('dstCountry')?.setValue(this.apiService.countries[2])
   }
 
   convert() {
@@ -50,10 +37,7 @@ export class ConvertComponent implements OnInit {
         this.exform.get('value')?.value
       )
       .subscribe((response) => {
-        console.log(response);
         this.exform.controls['amount'].setValue(response);
-        console.log(response);
-        console.log(this.conversionOutput);
       });
   }
 
