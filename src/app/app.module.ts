ecb3ac6f61ee3e24f7fce7df33db973a27da2d4d
abdelrahman amgad <abdelrhmanamggad14@gmail.com>
2023-08-29 16:27:20 +0300

    spinnner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ErrorInterceptor } from './core/interceptors/error-catching.interceptor';
-import { NavigationFormComponent } from './components/navigation-form/navigation-form.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { ConvertComponent } from './components/convert/convert.component';
-import { DropdownModule } from 'primeng/dropdown';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { ApiService } from './core/services/api.service';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
-import { RateCardComponent } from './components/rate-card/rate-card.component';
-import { CompareComponent } from './components/compare/compare.component';
-import { FavouritesComponent } from './components/favourites/favourites.component';
-import { KeyFilterModule } from 'primeng/keyfilter';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorInterceptor} from './core/interceptors/error-catching.interceptor';
+import {NavigationFormComponent} from './components/navigation-form/navigation-form.component';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {ConvertComponent} from './components/convert/convert.component';
+import {DropdownModule} from 'primeng/dropdown';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {ApiService} from './core/services/api.service';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
+import {RateCardComponent} from './components/rate-card/rate-card.component';
+import {CompareComponent} from './components/compare/compare.component';
+import {FavouritesComponent} from './components/favourites/favourites.component';
+import {KeyFilterModule} from 'primeng/keyfilter';
+import {SpinnerComponent} from './shared/spinner/spinner.component';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +46,7 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
     ButtonModule,
     ReactiveFormsModule,
     DynamicDialogModule,
-    KeyFilterModule
+    KeyFilterModule, ProgressSpinnerModule
   ],
   providers: [
     {
@@ -56,4 +58,5 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
